e4bdd66d0e9e1c48402ba2c41abfc881ea5ab7d6
akinalkan <103288322+akinalkan@users.noreply.github.com>
2022-07-06 21:34:25 +0300

    day18

--- src/Replit/C05karakterinIsimdeKacKezTekrarlandı.java
+++ src/Replit/C05karakterinIsimdeKacKezTekrarlandı.java
@@ -10,6 +10,7 @@ public class C05karakterinIsimdeKacKezTekrarlandı {
         String name=scan.nextLine();
         System.out.println("Lutfen bir karakter giriniz");
         char ch1=scan.next().charAt(0);
+
         int kontrol=0;
         for(int i=0 ; i<=name.length()-1; i++){
             if(ch1==name.charAt(i)){
